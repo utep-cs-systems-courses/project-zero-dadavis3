7f0f7c93acdf92c04f9639b70e3a40b7c9887e7f
student <dadavis3@miners.utep.edu>
2021-07-22 20:04:55 -0600

    Added Infinity sign and Poker Face

--- draw.h
+++ draw.h
@@ -4,9 +4,11 @@
 void print_triangle(int startCol, int size);
 void print_square(int startCol, int size);
 void print_char_11x16(char c);
+void print_arrow(int startCol, int size);
+void print_char_8x12(char c);
 
-extern const unsigned short font_11x16[95][11];
+extern const unsigned short font_11x16[96][11];
 extern const unsigned char font_5x7[96][5];
-extern const unsigned char font_8x12[95][12];
+extern const unsigned char font_8x12[96][12];
 
 #endif
